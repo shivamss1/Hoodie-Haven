be6b569bc853cb62fc786b54172a3c8934a7905a
shivamss1 <shivamlimbhare9@gmail.com>
2024-09-01 12:07:13 +0530

    Project Backend Added

---
+++ Hoodie-Haven Backend/Hoodie-Haven-Backend/src/main/java/com/HoodieStore/controller/ProductController.java
@@ -0,0 +1,42 @@
+package com.HoodieStore.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.HoodieStore.model.Product;
+import com.HoodieStore.service.ProductServiceImp;
+
+@RestController
+public class ProductController {
+	
+	@Autowired
+	ProductServiceImp ps;
+	
+	@PostMapping("/add-product")
+	public Product addProduct(@RequestBody Product product) {
+		return ps.addproduct(product);
+	}
+	
+	@GetMapping("/products")
+	public List<Product> getProduct(){
+		return ps.getProduct();
+	}
+	
+	@DeleteMapping("/delete-product")
+	public String deleteProduct(@RequestParam("id") Long id) {
+		return ps.deleteproduct(id);
+	}
+	
+	@PutMapping("/update-stock")
+	public void updateStock(@RequestParam("quantity") int quantity,@RequestParam("id") Long id) {
+		ps.updateStock(quantity,id);
+	}
+}
---
+++ Hoodie-Haven Backend/Hoodie-Haven-Backend/src/main/java/com/HoodieStore/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.HoodieStore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.HoodieStore.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+
+}
